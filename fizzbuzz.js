39c5fc70df82d91680f40da2141106750077c870
Rabbi <32594624+skolom@users.noreply.github.com>
2025-02-21 15:48:08 +0000

    Defined a variable: answer to store user input

---
+++ fizzbuzz.js
@@ -0,0 +1,11 @@
+let answer = parseInt(prompt("Please Enter the number you would like to FizzBuzz up to:"));
+
+
+for (let i = answer; i <= answer; i++) {
+    if (i % 3 === 0) {
+        console.log("Fizz");
+      } else {
+        console.log(i);
+      }
+    
+}
